52456bea3e1a78b26d12a1214b268b54fe6a512c
Cynthia <ccastelgonzalez@safareyes.es>
2023-11-09 16:57:06 +0100

    3 ccg

--- Trabajo GIT.py
+++ Trabajo GIT.py
@@ -1,18 +1,18 @@
 #Representar matrices
 def actividad_6():
-    a = ((1, 2, 3),
+    a = ((3, 2, 3),
          (4, 5, 6))
     b = ((-1, 0),
          (0, 1),
          (1, 1))
     resultado = [[0, 0], [0, 0]]
-    for i in range(len(a)):
+    for x in range(len(a)):
         for j in range(len(b[0])):
             for k in range(len(b)):
                 resultado[i][j] += a[i][k]*b[k][j]
-    for i in range(len(resultado)):
+    for x in range(len(resultado)):
         resultado[i]= tuple(resultado[i])
     resultado = tuple(resultado)
-    for i in range(len(resultado)):
+    for x in range(len(resultado)):
         print(resultado[i])
-    #Este comentario se tiene que eliminar
+
