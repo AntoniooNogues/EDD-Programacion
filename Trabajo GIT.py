729f971e471341c304cb982b8ac571fedce0e3bf
Antonio Nogues <anoguesgomez@safareyes.es>
2023-11-09 16:42:24 +0100

    Fase 2 - ANG

--- Trabajo GIT.py
+++ Trabajo GIT.py
@@ -1,19 +1,7 @@
-#Representar matrices
+#Todo el proyecto a cambiado "Realizado por Antonio"
 def actividad_6():
-    a = ((1, 2, 3),
-         (4, 5, 6))
-    b = ((-1, 0),
-         (0, 1),
-         (1, 1))
-    resultado = [[0, 0], [0, 0]]
-    for i in range(len(a)):
-        for j in range(len(b[0])):
-            for k in range(len(b)):
-                resultado[i][j] += a[i][k]*b[k][j]
-    for i in range(len(resultado)):
-        resultado[i]= tuple(resultado[i])
-    resultado = tuple(resultado)
-    for i in range(len(resultado)):
-        print(resultado[i])
-    #Este comentario se tiene que eliminar
+    a = 2+2
+    resultado = a**(9*a)
+    print(resultado)
+actividad_6()
 
