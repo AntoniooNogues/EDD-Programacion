5d24633ca0b788bbb45d0279eecf44ab19bd1dc8
Lucia <lmerinodelgado@safareyes.es>
2023-11-09 17:16:33 +0100

    cambios

--- Trabajo GIT.py
+++ Trabajo GIT.py
@@ -1,13 +1,8 @@
-#Representar matrices
+#Todo el proyecto a cambiado "Realizado por Antonio"
 def actividad_6():
-    a = ((1, 2, 3),
-         (4, 5, 6))
-    b = ((-1, 0),
-         (0, 1),
-         (1, 1))
-    resultado = [[0, 0], [0, 0]]
-    for i in range(len(a)):
-        for l in range(len(b[0])):
+<<<<<<< HEAD
+
+
             for k in range(len(b)):
                 resultado[i][l] += a[i][k]*b[k][l]
     for i in range(len(resultado)):
@@ -26,3 +21,10 @@ def ejercicio3_(palabra):
     contador_total = contador_a + contador_A
 
     return contador_total
+=======
+    a = 2+2
+    resultado = a**(9*a)
+    print(resultado)
+actividad_6()
+
+>>>>>>> 729f971e471341c304cb982b8ac571fedce0e3bf
