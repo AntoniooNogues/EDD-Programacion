f6e894dd4517afc6128a6dadb1a6d96c0689b5ef
Cynthia <ccastelgonzalez@safareyes.es>
2023-11-09 17:16:10 +0100

    cynthx

--- Trabajo GIT.py
+++ Trabajo GIT.py
@@ -16,3 +16,23 @@ def actividad_6():
     for i in range(len(resultado)):
         print(resultado[i])
     #Este comentario se tiene que eliminar
+
+    numero = random.randint(1, 10)
+
+    intentos = 0
+
+    while intentos < 3:
+        intento = int(input("¡Bienvenido/a! Introduce el número que pienses que es:  "))
+        # Si el número es igual al número secreto. Gana.
+        if intento == numero:
+            print("¡Enhorabuena!, Adivinaste el número secreto.", numero)
+            break
+        elif intento < numero:
+            print("El número es mayor.")
+        else:
+            print("El número es menor.")
+
+        intentos += 1
+    # Si en el número de intentos no lo adivinas, se dice la solución.
+    if intentos == 3 and intento != numero:
+        print("Has agotado tus 3 intentos. El número secreto era:", numero)
